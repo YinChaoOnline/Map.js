b7d81879b14cfc7c0047965897f1be1032e2cb8b
yinchaoonline <ychaowhu@foxmail.com>
2016-08-09 15:33:29 +0800

    update html

--- js/app.js
+++ js/app.js
@@ -1,51 +1,44 @@
 var map;
 
 require([
-    "esri/map"
+    "esri/map",
+    "esri/dijit/BasemapToggle",
+    "esri/basemaps",
+    "esri/dijit/HomeButton",
+    "dojo/domReady"
+    ], function (Map, BasemapToggle, esriBasemaps, HomeButton) {
 
+    //define china vector layer
+    esriBasemaps.china = {
+        baseMapLayers: [{
+            url: "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineCommunity/MapServer"
+                }],
+        thumbnailUrl: "https://www.example.com/images/thumbnail_2014-11-25_61051.png",
+        title: "china"
+    };
 
-        
-        , "esri/dijit/BasemapToggle"
-
-        
-        , "esri/basemaps"
-        
-        , "esri/dijit/HomeButton"
-        
-        , "dojo/domReady"]
-    , function (Map, BasemapToggle, esriBasemaps, HomeButton) {
-
-        //define china vector layer
-        esriBasemaps.china = {
-            baseMapLayers: [{
-                url: "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineCommunity/MapServer"
-                }]
-            , thumbnailUrl: "https://www.example.com/images/thumbnail_2014-11-25_61051.png"
-            , title: "china"
-        };
-
-        map = new Map("map", {
-            basemap: "china"
-            , center: [110, 32]
-            , zoom: 4
-            , logo: false
-            , minZoom: 4
-            , slider: true
-        });
-
-        var basemapToggle = new BasemapToggle({
-            theme: "basemapToggle"
-            , map: map
-            , visible: true
-            , basemap: "hybrid"
-        }, "basemap-toggle");
-        basemapToggle.startup();
-
-        var homeButton = new HomeButton({
-            theme: "HomeButton"
-            , map: map
-            , visible: true
-        }, "btn-home");
-        homeButton.startup();
-
+    map = new Map("map", {
+        basemap: "china",
+        center: [110, 32],
+        zoom: 4,
+        logo: false,
+        minZoom: 4,
+        slider: true
     });
+
+    var basemapToggle = new BasemapToggle({
+        theme: "basemapToggle",
+        map: map,
+        visible: true,
+        basemap: "hybrid"
+    }, "basemap-toggle");
+    basemapToggle.startup();
+
+    var homeButton = new HomeButton({
+        theme: "HomeButton",
+        map: map,
+        visible: true
+    }, "btn-home");
+    homeButton.startup();
+
+});
